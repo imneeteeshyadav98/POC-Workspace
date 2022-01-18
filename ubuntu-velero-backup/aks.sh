0c9e49ba064cefb023de6e2c729e8c7658dba108
imneeteeshyadav98 <neeteeshyadav98@gmail.com>
2022-01-18 15:42:14 +0530

    update

--- ubuntu-velero-backup/aks.sh
+++ ubuntu-velero-backup/aks.sh
@@ -4,7 +4,7 @@ VELERO_RESOURCE_GROUP_NAME="k8s-resource-01"
 VELERO_STORAGE_ACCOUNT_NAME="k8svelerobackup01" 
 VELERO_BLOB_CONTAINER_NAME="k8svelerobackup"
 LOCATION="westus"
-VELERO_SP_NAME="velerobackup"
+VELERO_SP_NAME="k8svelerobackup"
 AKS_RESOURCE_GROUP="MC_k8s-resource-01_k8s-cluster-01_westus"
 
 
