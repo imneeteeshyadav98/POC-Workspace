0c962a4acdf9220815f574f177f27bee4a6c27f9
imneeteeshyadav98 <neeteeshyadav98@gmail.com>
2022-01-24 18:32:38 +0530

    velero-azure-identity

---
+++ Velero-Azure-Identity/aks.sh
@@ -0,0 +1,21 @@
+SUBSCRIPTION="307229a7-f578-4e4c-b96e-522183580ae3"
+VELERO_RESOURCE_GROUP_NAME="k8s-resource-01"
+VELERO_STORAGE_ACCOUNT_NAME="k8svelerobackup01" 
+VELERO_BLOB_CONTAINER_NAME="k8svelerobackup"
+LOCATION="westus"
+VELERO_SP_NAME="k8svelerobackup"
+AKS_RESOURCE_GROUP="MC_k8s-resource-01_k8s-cluster-01_westus"
+
+
+#Create a resource group for the backup storage account.
+echo "Creating resource group..."
+az account set --subscription $SUBSCRIPTION
+az group create --location $LOCATION --name $VELERO_RESOURCE_GROUP_NAME
+
+#Create the storage account.
+echo "Creating storage account..."
+az storage account create --name $VELERO_STORAGE_ACCOUNT_NAME --resource-group $VELERO_RESOURCE_GROUP_NAME --location $LOCATION --kind StorageV2 --sku Standard_GRS --encryption-services blob --https-only true --access-tier Hot
+  
+#Create Blob Container.
+echo "Creating Blob Container..."
+az storage container create --name $VELERO_BLOB_CONTAINER_NAME --public-access off --account-name $VELERO_STORAGE_ACCOUNT_NAME
