54a3fb511e7ef2931109e7366529bdb620d9f1bd
imneeteeshyadav98 <neeteeshyadav98@gmail.com>
2022-01-25 09:50:18 +0530

    initial

--- Velero-Azure-Identity/aks.sh
+++ Velero-Azure-Identity/aks.sh
@@ -24,22 +24,3 @@ AZURE_RESOURCE_GROUP=${AZURE_RESOURCE_GROUP}
 AZURE_CLOUD_NAME=AzurePublicCloud
 EOF
 
-#Create a resource group for the backup storage account.
-echo "Creating resource group..."
-az account set --subscription $SUBSCRIPTION
-az group create --location $LOCATION --name $VELERO_RESOURCE_GROUP_NAME
-
-#Create the storage account.
-echo "Creating storage account..."
-az storage account create --name $VELERO_STORAGE_ACCOUNT_NAME --resource-group $VELERO_RESOURCE_GROUP_NAME --location $LOCATION --kind StorageV2 --sku Standard_GRS --encryption-services blob --https-only true --access-tier Hot
-  
-#Create Blob Container.
-echo "Creating Blob Container..."
-az storage container create --name $VELERO_BLOB_CONTAINER_NAME --public-access off --account-name $VELERO_STORAGE_ACCOUNT_NAME
-
-AZURE_STORAGE_ACCOUNT_ACCESS_KEY=`az storage account keys list --account-name $AZURE_STORAGE_ACCOUNT_ID --query "[?keyName == 'key1'].value" -o tsv`
-
-cat << EOF  > ./credentials-velero
-AZURE_STORAGE_ACCOUNT_ACCESS_KEY=${AZURE_STORAGE_ACCOUNT_ACCESS_KEY}
-AZURE_CLOUD_NAME=AzurePublicCloud
-EOF
--- Velero-Azure-Identity/identity.sh
+++ Velero-Azure-Identity/identity.sh
@@ -1,20 +1,20 @@
-cat <<EOF | kubectl apply -f -
-apiVersion: "aadpodidentity.k8s.io/v1"
-kind: AzureIdentity
-metadata:
-  name: $IDENTITY_NAME
-spec:
-  type: 0
-  resourceID: $IDENTITY_RESOURCE_ID
-  clientID: $IDENTITY_CLIENT_ID
-EOF
+#Create a resource group for the backup storage account.
+echo "Creating resource group..."
+az account set --subscription $SUBSCRIPTION
+az group create --location $LOCATION --name $VELERO_RESOURCE_GROUP_NAME
 
-cat <<EOF | kubectl apply -f -
-apiVersion: "aadpodidentity.k8s.io/v1"
-kind: AzureIdentityBinding
-metadata:
-  name: $IDENTITY_NAME-binding
-spec:
-  azureIdentity: $IDENTITY_NAME
-  selector: $IDENTITY_NAME
+#Create the storage account.
+echo "Creating storage account..."
+az storage account create --name $VELERO_STORAGE_ACCOUNT_NAME --resource-group $VELERO_RESOURCE_GROUP_NAME --location $LOCATION --kind StorageV2 --sku Standard_GRS --encryption-services blob --https-only true --access-tier Hot
+  
+#Create Blob Container.
+echo "Creating Blob Container..."
+az storage container create --name $VELERO_BLOB_CONTAINER_NAME --public-access off --account-name $VELERO_STORAGE_ACCOUNT_NAME
+
+AZURE_STORAGE_ACCOUNT_ACCESS_KEY=`az storage account keys list --account-name $AZURE_STORAGE_ACCOUNT_ID --query "[?keyName == 'key1'].value" -o tsv`
+
+
+cat << EOF  > ./credentials-velero
+AZURE_STORAGE_ACCOUNT_ACCESS_KEY=${AZURE_STORAGE_ACCOUNT_ACCESS_KEY}
+AZURE_CLOUD_NAME=AzurePublicCloud
 EOF
