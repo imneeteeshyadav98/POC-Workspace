8afa398b47be9c62e54ecaec7854678baf175063
imneeteeshyadav98 <neeteeshyadav98@gmail.com>
2022-02-01 14:22:41 +0530

    react

--- aks-poc/test-app/src/Button.js
+++ aks-poc/test-app/src/Button.js
@@ -4,8 +4,8 @@ import './App.css';
 function Button() {
   return (
     <div className="App">
-        <button>Custom Button 1</button>
-        <button>Custom Button 2</button>
+        <button onclick="http://13.88.57.107:5000/healthcheckup">Custom Button 1</button>
+        <button onclick="http://13.87.204.208:5000/hello">Custom Button 2</button>
     </div>
   );
 }
