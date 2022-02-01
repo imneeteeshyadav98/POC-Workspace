2de888723be36523349a3900531aa85b35c42fbd
imneeteeshyadav98 <neeteeshyadav98@gmail.com>
2022-02-01 23:10:21 +0530

    react

--- aks-poc/test-app/src/Button.js
+++ aks-poc/test-app/src/Button.js
@@ -8,7 +8,7 @@ function Button() {
         <a href="http://104.42.186.71:5000/api/v1/button/button2">Button 2</a><br/>
         <a href="http://40.78.9.231:5000/api/v1/button/button3">Button 3</a><br/>
         <a href="http://40.78.9.231:5000/api/v1/button/button4">Button 4</a><br/>
-        <a href="http://40.78.9.231:5000/api/v1/button/button5">Button 4</a>
+        <a href="http://40.78.9.231:5000/api/v1/button/button5">Button 5</a>
 
     </div>
   );
