f8b5ce9a6d6321795dd52c8d7db667fa7bcf612a
imneeteeshyadav98 <neeteeshyadav98@gmail.com>
2022-02-01 16:59:18 +0530

    react

--- aks-poc/test-app/src/Button.js
+++ aks-poc/test-app/src/Button.js
@@ -4,10 +4,10 @@ import './App.css';
 function Button() {
   return (
     <div className="App">
-        <a href="http://13.73.39.46:5000/button1">Button 1</a><br/>
-        <a href="http://13.73.39.46:5000/button2">Button 2</a><br/>
-        <a href="http://13.64.19.163:5000/button3">Button 3</a><br/>
-        <a href="http://13.64.19.163:5000/button4">Button 4</a>
+        <a href="http://13.87.188.87:5000/button1">Button 1</a><br/>
+        <a href="http://13.87.188.87:5000/button2">Button 2</a><br/>
+        <a href="http://40.83.168.246:5000/button3">Button 3</a><br/>
+        <a href="http://40.83.168.246:5000/button4">Button 4</a>
     </div>
   );
 }
