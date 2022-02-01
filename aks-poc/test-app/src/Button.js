9ae3221efbae657ae0adb720b7cc404376ee09c7
imneeteeshyadav98 <neeteeshyadav98@gmail.com>
2022-02-01 14:09:07 +0530

    react

---
+++ aks-poc/test-app/src/Button.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Button() {
+  return (
+    <div className="App">
+        <button>Custom Button</button>
+    </div>
+  );
+}
+
+export default Button;
