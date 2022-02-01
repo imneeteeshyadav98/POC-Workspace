926b82ad087f1cbf9d70ce32b16eb0358bc2cdc4
imneeteeshyadav98 <neeteeshyadav98@gmail.com>
2022-02-01 12:47:22 +0530

    flask

--- aks-poc/dockerinstall.sh
+++ aks-poc/dockerinstall.sh
@@ -3,5 +3,5 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt install docker-ce
-sudo systemctl status docker
+
 
