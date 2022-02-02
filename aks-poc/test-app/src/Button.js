caff43992e987133e4f09d078e9e1d1be4da8a1b
imneeteeshyadav98 <neeteeshyadav98@gmail.com>
2022-02-02 10:27:00 +0530

    api

--- aks-poc/test-app/src/Button.js
+++ aks-poc/test-app/src/Button.js
@@ -8,7 +8,8 @@ function Button() {
         <a href="http://52.160.88.32:5000/api/v2/microservice1/button/button2">Button 2</a><br/>
         <a href="http://13.88.28.111:5000/api/v2/microservice2/button/button3">Button 3</a><br/>
         <a href="http://13.88.28.111:5000/api/v2/microservice2/button/button4">Button 4</a><br/>
-        <a href="http://13.88.28.111:5000/api/v2/microservice2/button/button5">Button 5</a>
+        <a href="http://13.88.28.111:5000/api/v2/microservice2/button/button5">Button 5</a><br/>
+        <a href="http://13.88.28.111:5000/api/v2/microservice2/button/button6">Button 6</a>
 
     </div>
   );
