ebe50e9b53b66f7e880f4a844fbed22a2b89b699
imneeteeshyadav98 <neeteeshyadav98@gmail.com>
2022-02-03 14:26:41 +0530

    update

--- aks-poc/test-app/src/Button.js
+++ aks-poc/test-app/src/Button.js
@@ -4,8 +4,8 @@ import './App.css';
 function Button() {
   return (
     <div className="App">
-        <a href="http://52.160.88.32:80/api/v2/microservices1/button/button1">Button 1</a><br/>
-        <a href="http://52.160.88.32:80/api/v2/microservices1/button/button2">Button 2</a><br/>
+        <a href="http://40.112.255.76:80/api/v2/microservices1/button/button1">Button 1</a><br/>
+        <a href="http://40.112.255.76:80/api/v2/microservices1/button/button2">Button 2</a><br/>
         <a href="http://40.112.255.76:80/api/v2/microservices2/button/button3">Button 3</a><br/>
         <a href="http://40.112.255.76:80/api/v2/microservices2/button/button4">Button 4</a><br/>
         <a href="http://40.112.255.76:80/api/v2/microservices2/button/button5">Button 5</a><br/>
