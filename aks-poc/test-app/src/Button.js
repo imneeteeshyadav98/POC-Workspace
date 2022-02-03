c8bf891c7a5a12d32eba90a7dd3777e92af22ef6
imneeteeshyadav98 <neeteeshyadav98@gmail.com>
2022-02-03 11:29:19 +0530

    healthcheckup and helloapi update

--- aks-poc/test-app/src/Button.js
+++ aks-poc/test-app/src/Button.js
@@ -4,11 +4,11 @@ import './App.css';
 function Button() {
   return (
     <div className="App">
-        <a href="http://52.160.88.32:5000/api/v2/microservices1/button/button1">Button 1</a><br/>
-        <a href="http://52.160.88.32:5000/api/v2/microservices1/button/button2">Button 2</a><br/>
-        <a href="http://13.88.28.111:5000/api/v2/microservices2/button/button3">Button 3</a><br/>
-        <a href="http://13.88.28.111:5000/api/v2/microservices2/button/button4">Button 4</a><br/>
-        <a href="http://13.88.28.111:5000/api/v2/microservices2/button/button5">Button 5</a><br/>
+        <a href="http://52.160.88.32:80/api/v2/microservices1/button/button1">Button 1</a><br/>
+        <a href="http://52.160.88.32:80/api/v2/microservices1/button/button2">Button 2</a><br/>
+        <a href="http://13.88.28.111:80/api/v2/microservices2/button/button3">Button 3</a><br/>
+        <a href="http://13.88.28.111:80/api/v2/microservices2/button/button4">Button 4</a><br/>
+        <a href="http://13.88.28.111:80/api/v2/microservices2/button/button5">Button 5</a><br/>
 
     </div>
   );
