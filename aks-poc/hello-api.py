222f193e5f6917f6edff45d4dc92e69927c11529
imneeteeshyadav98 <neeteeshyadav98@gmail.com>
2022-02-03 11:11:32 +0530

    helloapi

--- aks-poc/hello-api.py
+++ aks-poc/hello-api.py
@@ -54,6 +54,6 @@ def button5():
     app.logger.info(response_data)
     return jsonify(response_data)
 if __name__ == '__main__':
-	app.run(host="0.0.0.0",port=5000,debug=True) 
+	app.run(host="0.0.0.0",port=80,debug=True) 
 
 
