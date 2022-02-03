9a88a891554697da25c53d4e973ae099bfc4bce3
imneeteeshyadav98 <neeteeshyadav98@gmail.com>
2022-02-03 23:49:51 +0530

    update

--- aks-poc/test-app/src/Button.js
+++ aks-poc/test-app/src/Button.js
@@ -4,13 +4,13 @@ import './App.css';
 function Button() {
   return (
     <div className="App">
-        <a href="http://40.78.89.253:80/api/v2/microservices1/button/button1">Button 1</a><br/>
-        <a href="http://40.78.89.253:80/api/v2/microservices1/button/button2">Button 2</a><br/>
-        <a href="http://40.78.89.253:80/api/v2/microservices2/button/button3">Button 3</a><br/>
-        <a href="http://40.78.89.253:80/api/v2/microservices2/button/button4">Button 4</a><br/>
-        <a href="http://40.78.89.253:80/api/v2/microservices2/button/button5">Button 5</a><br/>
-        <a href="http://40.78.89.253:80/healthcheckup">Healthcheckup 1</a><br/>
-        <a href="http://40.78.89.253:80/healthcheckup">Healthcheckup 2</a><br/>
+        <a href="http://testapp.appscoop.in/api/v2/microservices1/button/button1">Button 1</a><br/>
+        <a href="http://testapp.appscoop.in/api/v2/microservices1/button/button2">Button 2</a><br/>
+        <a href="http://testapp.appscoop.in/api/v2/microservices2/button/button3">Button 3</a><br/>
+        <a href="http://testapp.appscoop.in/api/v2/microservices2/button/button4">Button 4</a><br/>
+        <a href="http://testapp.appscoop.in/api/v2/microservices2/button/button5">Button 5</a><br/>
+        <a href="http://testapp.appscoop.in/healthcheckup">Healthcheckup 1</a><br/>
+        <a href="http://testapp.appscoop.in/healthcheckup">Healthcheckup 2</a><br/>
 
     </div>
   );
