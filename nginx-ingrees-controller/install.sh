f04d636f61bb087984e5f92fddccfc4e6c1f8ed8
imneeteeshyadav98 <neeteeshyadav98@gmail.com>
2022-02-28 13:15:18 +0530

    ingress install

--- nginx-ingrees-controller/install.sh
+++ nginx-ingrees-controller/install.sh
@@ -1,5 +1,5 @@
-kubectl create namespace ingress
-NAMESPACE=ingress
+kubectl create namespace webapp-react-nginx
+NAMESPACE=webapp-react-nginx
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 helm install ingress-nginx ingress-nginx/ingress-nginx --create-namespace --namespace $NAMESPACE
